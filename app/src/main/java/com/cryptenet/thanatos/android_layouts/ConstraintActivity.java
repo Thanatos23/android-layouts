692dabe6e51b621f98682f592c4dc32473b80144
Alamgir Hossain <al.hossain94@gmail.com>
2018-07-19 05:27:58 +0600

    layouts added

---
+++ app/src/main/java/com/cryptenet/thanatos/android_layouts/ConstraintActivity.java
@@ -0,0 +1,51 @@
+package com.cryptenet.thanatos.android_layouts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ConstraintActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText etName, etGender, etOccupation, etEmail, etContact, etHobby;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+
+        init();
+    }
+
+    private void init() {
+        viewBinder();
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void viewBinder() {
+        toolbar = findViewById(R.id.toolbar);
+
+        etName = findViewById(R.id.et_name);
+        etGender = findViewById(R.id.et_gender);
+        etOccupation = findViewById(R.id.et_occupation);
+        etEmail = findViewById(R.id.et_email);
+        etContact = findViewById(R.id.et_contact);
+        etHobby = findViewById(R.id.et_hobby);
+    }
+
+    public void formSubmitted(View v) {
+        String data =
+                etName.getText().toString() + "\n"
+                        + etGender.getText().toString() + "\n"
+                        + etOccupation.getText().toString() + "\n"
+                        + etEmail.getText().toString() + "\n"
+                        + etContact.getText().toString() + "\n"
+                        + etHobby.getText().toString();
+
+        Toast.makeText(ConstraintActivity.this, data, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/cryptenet/thanatos/android_layouts/MainActivity.java
+++ app/src/main/java/com/cryptenet/thanatos/android_layouts/MainActivity.java
@@ -1,22 +1,65 @@
 package com.cryptenet.thanatos.android_layouts;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Toolbar toolbar;
+    private Button btnToLinear, btnToRelative, btnToComposite, btnToConstraint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        init();
+    }
+
+    private void init() {
+        viewBinder();
+
         setSupportActionBar(toolbar);
+
+        btnToLinear.setOnClickListener(this);
+        btnToRelative.setOnClickListener(this);
+        btnToComposite.setOnClickListener(this);
+        btnToConstraint.setOnClickListener(this);
+    }
+
+    private void viewBinder() {
+        toolbar = findViewById(R.id.toolbar);
+
+        btnToLinear = findViewById(R.id.btn_to_linear);
+        btnToRelative = findViewById(R.id.btn_to_relative);
+        btnToComposite = findViewById(R.id.btn_to_composite);
+        btnToConstraint = findViewById(R.id.btn_to_constraint);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btn_to_linear:
+                intent = new Intent(MainActivity.this, LinearActivity.class);
+                break;
+            case R.id.btn_to_relative:
+                intent = new Intent(MainActivity.this, RelativeActivity.class);
+                break;
+            case R.id.btn_to_composite:
+                intent = new Intent(MainActivity.this, CompositeActivity.class);
+                break;
+            case R.id.btn_to_constraint:
+                intent = new Intent(MainActivity.this, LinearLayout.class);
+                break;
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
